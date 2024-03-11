3d6dcaaa51f74c5919f44592fd1c49d0e525727f
Szymon Kryzel <szymonkryzel13@gmail.com>
2024-03-11 23:04:09 +0100

    Add repository

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+using PlatformServise.Models;
+
+namespace PlatformServise.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
